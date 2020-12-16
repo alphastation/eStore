71092eacacd4e4e471991fa96bbd6d80978b4b3a
alphastation <alexander111111@gmail.com>
2020-12-16 16:10:04 +0000

    navigation menu and corresponding routing are ready

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
@@ -10,12 +11,16 @@ const routes: Routes = [
     path: '',
     component: MainLayoutComponent,
     children: [
-      {path: '', redirectTo: '/', pathMatch: 'full'},
+      { path: '', redirectTo: '/', pathMatch: 'full' },
       { path: '', component: MainPageComponent },
       { path: 'product/:id', component: ProductPageComponent },
       { path: 'cart', component: CartPageComponent },
     ],
   },
+  {
+    path: 'admin', loadChildren: './admin/admin.module#AdminModule'
+
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainLayoutComponent } from './shared/main-layout/main-layout.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ProductPageComponent } from './product-page/product-page.component';
 import { CartPageComponent } from './cart-page/cart-page.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
   declarations: [
@@ -18,7 +18,8 @@ import { CartPageComponent } from './cart-page/cart-page.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AdminModule
   ],
   providers: [],
   bootstrap: [AppComponent]
